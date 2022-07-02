0bb2ecb2061c614ce0e7d6728ced741cd2900d34
Drew <drewm727@hotmail.com>
2022-07-02 13:19:46 -0400

    Update clean.py

--- clean.py
+++ clean.py
@@ -6,20 +6,18 @@ try:
     (scriptname, directory, orgnzbname, jobname, reportnumber, category, group, postprocstatus, url) = sys.argv
 except:
     try:
-        directory = sys.argv[1]
-        jobname = sys.argv[3]
+	    directory = sys.argv[1]
+	    jobname = sys.argv[3]
     except:
         print("No commandline parameters found")
         sys.exit(1)
 
-DOCKERRUN=f"docker run --rm -v {directory}:/clean:rw -v /opt/plexcleaner:/config ptr727/plexcleaner /PlexCleaner/PlexCleaner --settingsfile /con
+DOCKERRUN=f"docker run --rm -v {directory}:/clean:rw -v /opt/plexcleaner:/config ptr727/plexcleaner /PlexCleaner/PlexCleaner --settingsfile /config/PlexCleaner.json --logfile /config/PlexCleaner.log process --mediafiles /clean --parallel --threadcount 10"
 
 print(DOCKERRUN)
 
 with open("/tmp/output.log", "a") as output:
     subprocess.call(DOCKERRUN, shell=True, stdout=output, stderr=output)
-
-print("Clean completed successfully"
 
 # Success code
 sys.exit(0)
