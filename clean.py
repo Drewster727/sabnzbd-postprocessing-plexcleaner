d400654e763e44b564ad21988c4b4c697c9955d0
Drew <drewm727@hotmail.com>
2022-07-03 09:50:29 -0400

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 import sys
-import subprocess
+from subprocess import PIPE, run
 import time
+import math
 
 try:
     (scriptname, directory, orgnzbname, jobname, reportnumber, category, group, postprocstatus, url) = sys.argv
@@ -18,19 +19,22 @@ def time_convert(sec):
     sec = sec % 60
     hours = mins // 60
     mins = mins % 60
-    return "{0}h:{1}m:{2}s".format(int(hours),int(mins),sec)
-	
-DOCKERRUN=f"docker run --rm -v {directory}:/clean:rw -v /opt/plexcleaner:/config ptr727/plexcleaner /PlexCleaner/PlexCleaner --settingsfile /config/PlexCleaner.json --logfile /config/PlexCleaner.log process --mediafiles /clean --parallel --threadcount 10"
-print(DOCKERRUN)
+    return "{0}h:{1}m:{2}s".format(int(hours),int(mins),math.ceil(sec))
+
+logfile="/tmp/output.log"
+cmd=f"docker run --rm -v {directory}:/clean:rw -v /opt/plexcleaner:/config ptr727/plexcleaner /PlexCleaner/PlexCleaner --settingsfile /config/PlexCleaner.json --logfile /config/PlexCleaner.log process --mediafiles /clean --parallel --threadcount 10"
+print(cmd)
+print("--------------")
 start_time = time.time()
 
-with open("/tmp/output.log", "a") as output:
-    subprocess.call(DOCKERRUN, shell=True, stdout=output, stderr=output)
+result = run([cmd], stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+print(result.returncode, result.stdout, result.stderr)
 
 end_time = time.time()
 time_lapsed = end_time - start_time
 runtime = time_convert(time_lapsed)
 
+print("--------------")
 print(f"Clean succeeded ({runtime})")
 
 # Success code
