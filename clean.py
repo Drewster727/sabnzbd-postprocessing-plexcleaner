735d5d8390fef26e75cafe423a3cff9a727419d7
Drew <drewm727@hotmail.com>
2022-07-03 09:21:38 -0400

    Update clean.py

--- clean.py
+++ clean.py
@@ -7,18 +7,18 @@ try:
     (scriptname, directory, orgnzbname, jobname, reportnumber, category, group, postprocstatus, url) = sys.argv
 except:
     try:
-	directory = sys.argv[1]
-	jobname = sys.argv[3]
+        directory = sys.argv[1]
+        jobname = sys.argv[3]
     except:
         print("No commandline parameters found")
         sys.exit(1)
 
 def time_convert(sec):
- 	mins = sec // 60
-	sec = sec % 60
-	hours = mins // 60
-	mins = mins % 60
-	return "{0}h:{1}m:{2}s".format(int(hours),int(mins),sec)
+    mins = sec // 60
+    sec = sec % 60
+    hours = mins // 60
+    mins = mins % 60
+    return "{0}h:{1}m:{2}s".format(int(hours),int(mins),sec)
 	
 DOCKERRUN=f"docker run --rm -v {directory}:/clean:rw -v /opt/plexcleaner:/config ptr727/plexcleaner /PlexCleaner/PlexCleaner --settingsfile /config/PlexCleaner.json --logfile /config/PlexCleaner.log process --mediafiles /clean --parallel --threadcount 10"
 print(DOCKERRUN)
@@ -35,5 +35,3 @@ print(f"Clean succeeded ({runtime})")
 
 # Success code
 sys.exit(0)
-
-
