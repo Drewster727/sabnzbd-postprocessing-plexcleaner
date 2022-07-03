5578e81cf57064bf222338d8b8b81649b1b9ed75
Drew <drewm727@hotmail.com>
2022-07-03 07:27:45 -0400

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import subprocess
+import time
 
 try:
     (scriptname, directory, orgnzbname, jobname, reportnumber, category, group, postprocstatus, url) = sys.argv
@@ -12,14 +13,27 @@ except:
         print("No commandline parameters found")
         sys.exit(1)
 
+def time_convert(sec):
+  mins = sec // 60
+  sec = sec % 60
+  hours = mins // 60
+  mins = mins % 60
+  print("Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),sec))
+	
 DOCKERRUN=f"docker run --rm -v {directory}:/clean:rw -v /opt/plexcleaner:/config ptr727/plexcleaner /PlexCleaner/PlexCleaner --settingsfile /config/PlexCleaner.json --logfile /config/PlexCleaner.log process --mediafiles /clean --parallel --threadcount 10"
-
 print(DOCKERRUN)
+start_time = time.time()
 
 with open("/tmp/output.log", "a") as output:
     subprocess.call(DOCKERRUN, shell=True, stdout=output, stderr=output)
 
-print("Clean completed successfully")
+end_time = time.time()
+time_lapsed = end_time - start_time
+runtime = time_convert(time_lapsed)
+
+print(f"Clean succeeded ({runtime})")
 
 # Success code
 sys.exit(0)
+
+
