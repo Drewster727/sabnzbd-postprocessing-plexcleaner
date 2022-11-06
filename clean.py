b9fcc494f379742e9a968391578f67913c6ea078
Drewster727 <mcminn.drew@gmail.com>
2022-11-06 11:13:41 -0500

    only clean non h265

--- clean.py
+++ clean.py
@@ -3,7 +3,10 @@ import sys
 import time
 import math
 import shutil
+import re
 from subprocess import PIPE, run
+
+cleanPattern = '[HX].?265|HEVC'
 
 def exec_docker(directory):
     print("--------------")
@@ -32,34 +35,17 @@ def move_dir(source, destination):
     print(f"Moving to {target}")
     shutil.move(source, target)
 
-def clean(directory, category):
-    # configure
-    # dir_destination="/media/tower/Storage/Completed"
-    # category_destination_dir_map={
-    #     "tv":f"{dir_destination}/_tv",
-    #     "movies":f"{dir_destination}/_movies",
-    #     "music":f"{dir_destination}/_music"
-    # }
-    # cat=category.lower()
-    # dir_destination_final=dir_destination
-    # if cat in category_destination_dir_map:
-    #     dir_destination_final=category_destination_dir_map[cat]
-
+def clean(directory, pattern):
     # clean
-    clean_start_time = time.time()
-    exec_docker(directory)
-    clean_end_time = time.time()
-    clean_runtime = time_convert(clean_end_time - clean_start_time)
-
-    # move
-    # mv_start_time = time.time()
-    # move_dir(directory, dir_destination_final)
-    # mv_end_time = time.time()
-    # move_runtime = time_convert(mv_end_time - mv_start_time)
-    # print(f"Move succeeded ({move_runtime})")
-
-    # Success code
-    print(f"Clean succeeded ({clean_runtime})")
+    if not re.search(pattern, directory, re.IGNORECASE):
+        clean_start_time = time.time()
+        exec_docker(directory)
+        clean_end_time = time.time()
+        clean_runtime = time_convert(clean_end_time - clean_start_time)
+        print(f"Clean succeeded ({clean_runtime})")
+    else:
+        print("Skipping clean. x265/HEVC found in title!")
+    
     sys.exit(0)
 
 try:
@@ -72,4 +58,4 @@ except:
         print("No commandline parameters found")
         sys.exit(1)
 
-clean(directory, category)
+clean(directory, cleanPattern)
